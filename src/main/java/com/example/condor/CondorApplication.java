f3223c437d3371e56853d5c079de4ef7e7131f64
yeongsang2 <dudtkd1221@gmail.com>
2023-12-02 22:06:54 +0900

    init

---
+++ src/main/java/com/example/condor/CondorApplication.java
@@ -0,0 +1,13 @@
+package com.example.condor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CondorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CondorApplication.class, args);
+    }
+
+}
